65d9fffeeb9a8183ea03a77dc6cfb0b4955a7c4b
45inertia <5myero37@solent.ac.uk>
2024-10-28 11:38:57 +0000

    Modified ex2.kt to ask the user how many times to reprint the name

--- wk1_helloworld/helloworld/src/ex2.kt
+++ wk1_helloworld/helloworld/src/ex2.kt
@@ -1,7 +1,10 @@
 fun main() {
+    println("How many times would you like to enter your name: ")
+    val sNum1 = readLine()
+    val nNum1 = sNum1?.toInt() ?: 0
     println("Please enter your name: ")
     val name = readLine()
-    for (i in 1..10) {
+    for (i in 1..nNum1) {
         println(name)
     }
 }
